170c81400d6b6a52613384879d43d5eefd4765ef
Jack Howard <johncarverhoward@gmail.com>
2017-07-03 11:48:19 -0500

    reaffix buttons

--- messenger/ViewController.swift
+++ messenger/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
